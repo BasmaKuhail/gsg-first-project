7820124af3e8075483703b764c0fd6207c29b2da
BasmaKuhail <basmakuhail2003@gmail.com>
2025-09-30 14:12:37 +0300

    ex1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import SideNav from './components/sidenav'
 import ToDoLsit from './components/person'
-import Profile from './components/Profile'
+import Profile from './components/excersises/Ex1'
 function App() {
 
   return (
