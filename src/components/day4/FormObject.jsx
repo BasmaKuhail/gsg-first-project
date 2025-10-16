d26c835f83c72976a0abedf4a71fb4a3c59a2fbe
BasmaKuhail <basmakuhail2003@gmail.com>
2025-10-16 13:57:12 +0300

    edit last commit

--- src/components/day4/FormObject.jsx
+++ src/components/day4/FormObject.jsx
@@ -6,6 +6,9 @@ export default function Form(){
         lname: "",
         email: ""
     })
+    function handleOnchange(e){
+        setData({...data, [e.target.name]: e.target.value})
+    }
     return(<form>
         <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
             <label>First name: {data.fname}</label>
@@ -13,9 +16,9 @@ export default function Form(){
             <label>Email:  {data.email}</label>
         </div>  
         <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
-            <input value={data.fname} placeholder="First name" onChange={(e) => setData({...data, fname: e.target.value})}/>
-            <input value={data.lname} placeholder="Last name" onChange={(e) => setData({...data, lname: e.target.value})}/>
-            <input value={data.email} placeholder="email" onChange={(e) => setData({...data, email: e.target.value})}/>
+            <input name = "fname" value={data.fname} placeholder="First name" onChange={handleOnchange}/>
+            <input name = "lname" value={data.lname} placeholder="Last name" onChange={handleOnchange}/>
+            <input name = "email" value={data.email} placeholder="email" onChange={handleOnchange}/>
         </div>
     </form>
     )
