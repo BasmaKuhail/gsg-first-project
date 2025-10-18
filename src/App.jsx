ff419645e9903316aa882b3454c5cc84e84bdd56
BasmaKuhail <basmakuhail2003@gmail.com>
2025-10-18 20:45:32 +0300

    map an array with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,12 @@
-import { useState } from "react";
-import Background from "./components/excersises-d5/Background";
-import Box from "./components/excersises-d5/Ex11";
+import { useState } from 'react'
+import './App.css'
 
-const initialPosition = {
-  x: 0,
-  y: 0,
-};
-
-export default function App() {
-  const [shape, setShape] = useState({
-    color: "orange",
-    position: initialPosition,
-  });
-
-  function handleMove(dx, dy) {
-    initialPosition = {x: initialPosition.x+=dx, y:initialPosition.y+=dy}
-    setShape({...shape, position:{initialPosition}})
-    // shape.position.x += dx;
-    // shape.position.y += dy;
-  }
-
-  function handleColorChange(e) {
-    console.log(shape.position)
-    setShape({
-      ...shape,
-      color: e.target.value,
-    });
-  }
+import ShapeEditor from './components/day5/TransformArray'
+function App() {
 
   return (
-    <>
-      <select value={shape.color} onChange={handleColorChange}>
-        <option value="orange">orange</option>
-        <option value="lightpink">lightpink</option>
-        <option value="aliceblue">aliceblue</option>
-      </select>
-      <Background position={initialPosition} />
-      <Box color={shape.color} position={shape.position} onMove={handleMove}>
-        Drag me!
-      </Box>
-    </>
-  );
+    <ShapeEditor/>
+  )
 }
+
+export default App
