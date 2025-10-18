9684d0bb94b8dee305b97a596b2e6f872b7298c5
BasmaKuhail <basmakuhail2003@gmail.com>
2025-10-18 13:42:00 +0300

    use immer

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 import Calculate from './components/day4/Calculator'
+import NestedObj from './components/day5/UpdateNestedObj'
 function App() {
 
   return (
-    <Calculate/>
+    <NestedObj/>
   )
 }
 
